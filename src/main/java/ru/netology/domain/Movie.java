c0a706204e8c4ef5433d6f6210895b9a5d4dc5a1
Pavel Stetsurin <qqqqqp00@gmail.com>
2022-05-01 16:36:05 +0200

    first commit

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+
+public class Movie {
+    private int id;
+    private String title;
+    private String genre;
+}
